f7ca8739bac928f3100d0852c78e57db6c46ac5e
Mahesh Dhulipudi <24P31F00A8@acet.ac.in>
2025-10-30 15:26:14 +0530

    Remove greeting from Footer component
    Removed the greeting 'hello' from the footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Footer = () => (
   <footer className="text-center py-6 bg-black bg-opacity-70 ">
     <p> {new Date().getFullYear()} Mahesh Dhulipudi.</p>
-    <h1>hello</h1>
   </footer>
 );
 
